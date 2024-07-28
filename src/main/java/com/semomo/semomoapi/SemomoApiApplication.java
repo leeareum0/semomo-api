dd9883bbd10a12f68bfe972394ed812fcdfc12d0
이아름 <areum6321@gmail.com>
2024-07-28 15:47:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/semomo/semomoapi/SemomoApiApplication.java
@@ -0,0 +1,13 @@
+package com.semomo.semomoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemomoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemomoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semomo/semomoapi/SemomoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semomo.semomoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemomoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
